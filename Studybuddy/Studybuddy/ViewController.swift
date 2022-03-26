6cf24e570b93ad88b07969fc34fee9f53402541f
Shiv <81172033+shivpvtel@users.noreply.github.com>
2022-03-26 14:41:50 -0400

    Add files via upload

---
+++ Studybuddy/Studybuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studybuddy
+//
+//  Created by Shiv Patel on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
