94e3d04156f6b5815ab6da96b12b8205bcafc7ca
sainyelaman <sain.yelaman@gmail.com>
2022-04-05 23:29:21 -0400

    Implemented login and sign up
    Implemented login and sign up feature with Parse. Created the login page view controller and home screen view controller

---
+++ Studybuddy/Studybuddy/LoginViewController.swift
@@ -0,0 +1,73 @@
+//
+//  LoginViewController.swift
+//  Studybuddy
+//
+//  Created by Yelaman Sain on 4/5/22.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    
+    
+    @IBAction func onSignin(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+          if user != nil {
+              self.performSegue(withIdentifier: "loginSegue", sender: nil)
+          } else {
+              print("Error: \(String(describing: error?.localizedDescription))")
+          }
+        }
+    }
+    
+    
+    @IBAction func onSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        
+        let imageName = "image_placeholder.png"
+        let image = UIImage(named: imageName)!
+        
+        let imageData = image.pngData()!
+        let imageFile = PFFileObject(name:"image_placeholder.png", data:imageData)
+
+        user["profilePic"] = imageFile
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
